d1e53fad97717e314dcba5980736fc06ca760ebe
HongTang2912 <77006112+HongTang2912@users.noreply.github.com>
2022-02-12 20:11:16 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 import Chat from "./Chat";
 
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://app-chat-nodejs-server.herokuapp.com/");
 
 function App() {
   const [username, setUsername] = useState("");
